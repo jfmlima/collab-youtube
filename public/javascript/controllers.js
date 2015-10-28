2c556640a5900ba8abe3e98c88cfb07eb7d13905
Jorge Lima <ei10102@fe.up.pt>
2015-10-28 23:29:53 +0100

    Created modal for joining rooms

--- public/javascript/controllers.js
+++ public/javascript/controllers.js
@@ -4,7 +4,7 @@
 
 angular.module('collabYoutube.controllers', [])
 
-    .controller('mainController', function($scope, $window, $http, $socket, $session, $collab) {
+    .controller('mainController', function($scope, $window, $http, $socket, $session, $collab, $uibModal, $log) {
         $scope.formData = {};
 
         $socket.on("connect", function(){
@@ -16,11 +16,52 @@ angular.module('collabYoutube.controllers', [])
             $session.setUser(null);
         }
 
-        $scope.joinRoom = function(){
+
+
+        $scope.items = ['item1', 'item2', 'item3'];
+
+        $scope.animationsEnabled = true;
+
+        $scope.joinRoom = function(size){
+
             $collab.join();
-        }
+
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: '/partials/joinModal',
+                controller: 'ModalInstanceCtrl',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
 
 
+    })
+
+    .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $uibModalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
     })
 
     .controller('loginController', function($scope, $window, $http, $socket) {
