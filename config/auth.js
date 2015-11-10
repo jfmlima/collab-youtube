23b9e60dc2ecad675660b231e7f9695b1b27d506
Jorge Lima <ei10102@fe.up.pt>
2015-11-10 20:14:46 +0100

    new callback

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1644709235772142', // your App ID
         'clientSecret'  : 'e3034764667aef8ceb4219964538ab16', // your App Secret
-        'callbackURL'   : 'https://collabyoutube.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : '/auth/facebook/callback'
     }
 
     /*'twitterAuth' : {
